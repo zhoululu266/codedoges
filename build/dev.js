179ea455c2542fabce2e9bf349f088c830bbcfac
石坤 <492809110@qq.com>
2017-06-27 10:30:55 +0800

    fixes

--- build/dev.js
+++ build/dev.js
@@ -3,14 +3,26 @@ import webpack from 'webpack'
 import {devMiddleware, hotMiddleware} from 'koa-webpack-middleware'
 import webpackConfig from '../configs/dev.babel.js'
 import koa from 'koa'
+import koaRouter from 'koa-router'
 import ora from 'ora'
-const spinner = ora('webpack-dev-server is starting')
+import fs from 'fs'
+let spinner = ora('webpack-dev-server is starting'),
+	router = new koaRouter(),
+	_resolve
+new Promise(resolve => {
+	_resolve = resolve
+})
 spinner.start()
-	const compiler = webpack(webpackConfig, (err, stats) => {
+const compiler = webpack(webpackConfig, (err, stats) => {
 		if (err)
 			throw err
-			spinner.stop()
+		spinner.stop()
+		_resolve()
 		process.stdout.write(stats.toString({colors: true, modules: false, children: false, chunks: false, chunkModules: false}) + '\n\n')
 	}),
 	app = new koa()
-app.use(devMiddleware(compiler, {quiet: true})).use(hotMiddleware(compiler)).listen(80)
+router.get('/', async(ctx) => {
+	ctx.type = 'text/html'
+	ctx.body = await fs.readFileSync(path.resolve(__dirname, '../index.html')).toString('utf8')
+})
+app.use(router.routes()).use(router.allowedMethods()).use(devMiddleware(compiler, {quiet: true})).use(hotMiddleware(compiler)).listen(80)
