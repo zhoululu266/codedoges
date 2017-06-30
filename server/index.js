445fe9466663298530af9e8e754d8e6f38330be3
xuyuntong <1228442292@qq.com>
2017-06-30 17:30:08 +0800

    usage of koa-session

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ import path from 'path'
 import koa from 'koa'
 import routes from './routes'
 import mongoose from 'mongoose'
-import session from 'koa-session'
 const NODE_ENV = process.env.NODE_ENV,
 	app = new koa(),
 	options = {
@@ -16,27 +15,6 @@ mongoose.connect('mongodb://localhost:27017/codedoges')
 mongoose.Promise = global.Promise
 
 routes(app)
-app.keys = ['some secret hurr'];
-
-const CONFIG = {
-	key: 'koa:sess',
-	/** (string) cookie key (default is koa:sess) */
-	/** (number || 'session') maxAge in ms (default is 1 days) */
-	/** 'session' will result in a cookie that expires when session/browser is closed */
-	/** Warning: If a session cookie is stolen, this cookie will never expire */
-	maxAge: 86400000,
-	overwrite: true,
-	/** (boolean) can overwrite or not (default true) */
-	httpOnly: true,
-	/** (boolean) httpOnly or not (default true) */
-	signed: true,
-	/** (boolean) signed or not (default true) */
-	rolling: false,
-	/** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. default is false **/
-};
-
-app.use(session(CONFIG, app));
-// or if you prefer all default config, just use => app.use(session(app));
 
 http.createServer(app.callback()).listen(8080)
 https.createServer(options, app.callback()).listen(443)
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,12 +5,34 @@ import koaRouter from 'koa-router'
 import multer from 'koa-multer'
 import path from 'path'
 import logger from 'koa-logger'
+import session from 'koa-session'
 let router = new koaRouter({prefix: '/api'}),
 	bodyParser = new koaBody({multipart: true}),
 	upload = multer({
 		dest: path.resolve(__dirname, '../data/test')
 	})
-let routes = app => { // post 用户登陆
+
+let routes = app => {
+	app.keys = ['some secret hurr'];
+
+	const CONFIG = {
+		key: 'koa:sess',
+		/** (string) cookie key (default is koa:sess) */
+		/** (number || 'session') maxAge in ms (default is 1 days) */
+		/** 'session' will result in a cookie that expires when session/browser is closed */
+		/** Warning: If a session cookie is stolen, this cookie will never expire */
+		maxAge: 86400000,
+		overwrite: true,
+		/** (boolean) can overwrite or not (default true) */
+		httpOnly: true,
+		/** (boolean) httpOnly or not (default true) */
+		signed: true,
+		/** (boolean) signed or not (default true) */
+		rolling: false,
+		/** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. default is false **/
+	};
+	router.use(session(CONFIG, app))
+	// post 用户登陆
 	router.post('/login', bodyParser, userController.login)
 	// post 用户注册
 	router.post('/signin', bodyParser, userController.signin)
@@ -20,6 +42,11 @@ let routes = app => { // post 用户登陆
 	router.get('/doubantop250', bodyParser, movieController.doubantop250)
 	// post 添加电影
 	router.post('/addmovie', bodyParser, movieController.addMovie)
+
+	router.get('/test', ctx => {
+		ctx.session.name = 'test'
+		ctx.body = 'test'
+	})
 
 	// 路由中间件
 	app.use(logger()).use(router.routes()).use(router.allowedMethods())
